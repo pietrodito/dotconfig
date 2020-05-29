a61646899f9bbdfb8fcc513185dda1de8604cd74
pietrodito <pietrodito@gmail.com>
2020-05-29 17:13:44 +0200

    Fetch lost files from older repo strategy

--- .oh-my-zsh/custom/general-aliases.zsh
+++ .oh-my-zsh/custom/general-aliases.zsh
@@ -3,4 +3,5 @@ alias md='mkdir -p'
 alias snow='systemctl poweroff -i'
 alias rnow='systemctl reboot -i'
 alias dodo='systemctl suspend -i'
+alias config='/usr/bin/git --git-dir=$HOME/Comp/dotconfig/ --work-tree=$HOME'
 gps () {ps -ax | grep $1}
