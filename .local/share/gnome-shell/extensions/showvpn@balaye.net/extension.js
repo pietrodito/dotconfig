263e7734798b136589fb31bf9d7d6e27fb187768
pietrodito <pietrodito@gmail.com>
2022-07-21 15:24:02 +0200

    Add context menu to ShowVPN extension

--- .local/share/gnome-shell/extensions/showvpn@balaye.net/extension.js
+++ .local/share/gnome-shell/extensions/showvpn@balaye.net/extension.js
@@ -1,43 +1,64 @@
-const { St, Clutter, GLib, Gio } = imports.gi;
+const GETTEXT_DOMAIN = 'my-indicator-extension';
+
+const { GObject, St, Clutter, GLib, Gio } = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
 
-let panelButton;
+const _ = ExtensionUtils.gettext;
 
-function init () {
+const Indicator = GObject.registerClass(
+class Indicator extends PanelMenu.Button {
 
-    const file = Gio.File.new_for_uri('file:///home/ulys/.config/wireguard/current_vpn');
 
-    function read_current_vpn () {
-        const [, contents, etag] = file.load_contents(null);
+    _read_current_vpn () {
+
+        const [, contents, etag] = this._file.load_contents(null);
         const decoder = new TextDecoder('utf-8');
         const contentsString = decoder.decode(contents).trim();
-        return "VPN: " + contentsString;
+        return "ShowVPN: " + contentsString ;
     }
 
-    panelButton = new St.Bin({
-        style_class : "panel-button",
-    });
-    let panelButtonText = new St.Label({
-        text : read_current_vpn(),
-        y_align: Clutter.ActorAlign.CENTER,
-    });
-    panelButton.set_child(panelButtonText);
+    _init() {
+        super._init(0.0, _('My Shiny Indicator'));
 
+        this._file = Gio.File.new_for_uri(
+             'file:///home/ulys/.config/wireguard/current_vpn');
+        let panelButtonText = new St.Label({
+                    text : this._read_current_vpn(),
+                    y_align: Clutter.ActorAlign.CENTER,
+                });
+        log('*--* après erreur *--* ');
+        this.add_child(panelButtonText);
 
-    const Mainloop = imports.mainloop;
-    let timeout = Mainloop.timeout_add_seconds(3, () => {
-        panelButtonText.set_text(read_current_vpn());
-        return true;
-    });
+        let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
+        item.connect('activate', () => {
+            Main.notify(_('Whatʼs up, folks?'));
+        });
+        this.menu.addMenuItem(item);
+    }
+});
 
+class Extension {
+    constructor(uuid) {
+        this._uuid = uuid;
+
+        ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+    }
+
+    enable() {
+        this._indicator = new Indicator();
+        Main.panel.addToStatusArea(this._uuid, this._indicator);
+    }
+
+    disable() {
+        this._indicator.destroy();
+        this._indicator = null;
+    }
 }
 
-function enable () {
-    // Add the button to the panel
-    Main.panel._rightBox.insert_child_at_index(panelButton, 0);
-}
-
-function disable () {
-    // Remove the added button from panel
-    Main.panel._rightBox.remove_child(panelButton);
+function init(meta) {
+    return new Extension(meta.uuid);
 }
